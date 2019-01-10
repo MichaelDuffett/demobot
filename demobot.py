44e0e2f7b6063fbd55e2bad1a58c67b3d7a5565e
Michael Duffett <mduf5470@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:22:06 +1100

    Added ask and return name command

--- demobot.py
+++ demobot.py
@@ -1,5 +1,5 @@
 # Import flask
-from flask import Flask
+from flask import Flask, request
 
 # Create your app (web server)
 app = Flask(__name__)
@@ -8,7 +8,12 @@ app = Flask(__name__)
 # When people visit the home page '/' use the hello_world function
 @app.route('/')
 def hello_world():
-    return 'Hello, World!. This is 2019. sd s d'
+    return 'Hello, World!. This is 2019.'
+
+@app.route('ask')
+def hello_world():
+    name = request.values.get("text")
+    return f"Hello {name}. Welcome to NCSS 2019 Group 1!"
 
 if __name__ == '__main__':
     # Start the web server!
